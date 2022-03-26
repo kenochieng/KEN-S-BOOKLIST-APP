0ca282bbbf3ea641d8b4fbcb7c36f1a260629ab5
kenochieng <71432010+kenochieng@users.noreply.github.com>
2022-03-26 11:18:22 +0300

    Add files via upload
    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//BOOK CLASS: CREATES A NEW BOOK EVERYTIME THIS class is instantiated
+class Book{
+    constructor(title,author,isbn){
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+
+//UI CLASS: Handles UI Tasks
+
+
+
+//Strore class: Handles local storage on the Browser
+
+
+//EVENT: Display Books
+
+
+//EVENT: Add a Book
+
+
+
+//EVENT: Remove a Book
+
